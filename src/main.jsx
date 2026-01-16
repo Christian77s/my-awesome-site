d3f33c4cb7f4d771bb380636a76988acc86b589d
ch1ns0n <samuel.chinson@gmail.com>
2026-01-16 15:26:48 +0700

    Fix deploy path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // Kita ganti ke HashRouter
 import App from './App.jsx'
 import './index.css'
 
-// Membuat root aplikasi dengan standar React 19
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* basename harus sama persis dengan nama repository GitHub kamu */}
-    <BrowserRouter basename="/my-awesome-site">
+    {/* Menggunakan HashRouter tidak memerlukan basename lagi */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    </HashRouter>
+  </React.StrictMode>,
+)
